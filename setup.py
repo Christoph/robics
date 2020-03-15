eba5e569d8057bb0e4ec68842245d943e0ec3cdd
Chris <christoph.kralj@gmail.com>
2020-03-15 17:55:12 +0100

    Added setup files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+    name='robics',
+    packages=['robics'],
+    version='0.1',
+    license='MIT',
+    description='Automatic detection of robust parametrizations for LDA and NMF. Compatible with scikit-learn and gensim.',
+    author='Christoph Kralj',
+    author_email='christoph.kralj@gmail.com',
+    url='https://github.com/Christoph/robics',
+    download_url='https://github.com/Christoph/robics/archive/v_01.tar.gz',
+    keywords=['nlp', 'Topic Model', 'sklearn', 'gensim', 'topic-modeling'],
+    install_requires=[            # I get to this in a second
+        'numpy',
+        'scipy',
+        'sobol_seq',
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Topic Modeling :: Robustness Testing',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
