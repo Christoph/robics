c1f9922c23aed1a0b14930e5f975afcb4484c019
Chris <christoph.kralj@gmail.com>
2020-03-15 18:06:28 +0100

    Updated classifier and removed manifest from git

--- setup.py
+++ setup.py
@@ -19,9 +19,8 @@ setup(
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Topic Modeling :: Robustness Testing',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        'Topic :: Text Processing',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
